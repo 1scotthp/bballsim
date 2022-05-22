3d5d04fa821ac118655e85d90f26bd59bb38f2a8
1scotthp <51714937+1scotthp@users.noreply.github.com>
2022-05-22 19:47:35 -0400

    first

---
+++ main.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+def run():
+    darko_data = pd.read_csv("data/darko.csv")
+
+
+    transition = pd.read_csv("data/transition.csv")
+    misc = pd.read_csv("data/misc.csv")
+    iso = pd.read_csv("data/iso.csv")
+    pick_and_roll_handler = pd.read_csv("data/pick_and_roll_handler.csv")
+    pick_and_roll_roller = pd.read_csv("data/pick_and_roll_roller.csv")
+    spotup = pd.read_csv("data/spotup.csv")
+    putback = pd.read_csv("data/putbacks.csv")
+    postup = pd.read_csv("data/postup.csv")
+
+    # res = darko_data.merge(transition,on=['PLAYER', 'Team'],how='outer')
+    res = transition.merge(misc,on=['PLAYER', 'TEAM'],how='outer')
+    res = res.merge(iso,on=['PLAYER', 'TEAM'],how='outer')
+    res = res.merge(pick_and_roll_handler, on=['PLAYER', 'TEAM'], how="outer")
+    res = res.merge(pick_and_roll_roller, on=['PLAYER', 'TEAM'], how="outer")
+    res = res.merge(spotup, on=['PLAYER', 'TEAM'], how="outer")
+    res = res.merge(putback, on=['PLAYER', 'TEAM'], how="outer")
+    res = res.merge(postup, on=['PLAYER', 'TEAM'], how="outer")
+    
+
+    res = darko_data.merge(res, on='PLAYER')
+    res.to_csv("master.csv")
+
+    # darko_data.to_csv("master.csv")
+
+    # transition.to_csv("t.csv")
+
+
+
+
+
+
+if __name__ == "__main__":
+    run()
